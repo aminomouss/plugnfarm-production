943fefdbd914f885536a7125e8e6fef3017fd6a9
Amine <anasafanif@gmail.com>
2018-11-03 23:29:06 +0100

    First production base

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module DeployTest
-module Tuto3
-  
+
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
