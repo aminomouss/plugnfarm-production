47ae48e28bc12c3ce6944fc9279dd4bca7762d2b
Amine <anasafanif@gmail.com>
2018-11-03 22:27:21 +0100

    Premiere modif

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "monsite"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "/home/user/git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
