d53afa1431f8fc30e176e390e01935f0e48f41cf
Amine <anasafanif@gmail.com>
2018-11-04 03:55:30 +0100

    again

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'plugnfarm.com', user: 'root', roles: %w{app web db}, ssh_options: { forward_agent: true }
+server 'www.plugnfarm.com', user: 'root', roles: %w{app web db}, ssh_options: { forward_agent: true }
 
 
 # role-based syntax
